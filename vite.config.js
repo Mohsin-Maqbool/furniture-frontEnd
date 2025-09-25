dc6bd5f0c481193cad36c2fc8693846bb9f2d207
Mohsin-Maqbool <mohsinmaqbool182@gmail.com>
2025-09-25 15:31:47 +0500

    Fix vite.config.js env loading

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default ({ mode }) => {
+  // Load env variables
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:4500", // fallback for local
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
-  server: {
-  proxy: {
-  "/api": import.meta.env.VITE_API_URL
-}
-}
-});
+  });
+};
